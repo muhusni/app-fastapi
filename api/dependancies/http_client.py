a1a32d42d227b01dbd2b7176404be275e5d1d10b
Muhamad Husni <unny.husni@gmail.com>
2024-07-22 00:16:58 +0700

    initial commit

---
+++ api/dependancies/http_client.py
@@ -0,0 +1,14 @@
+from typing import Annotated
+from fastapi import Depends
+from api.utils import AsyncHTTPClient
+
+# Dependency function to provide an instance of AsyncHTTPClient
+async def get_http_client() -> AsyncHTTPClient:
+    client = AsyncHTTPClient()
+    try:
+        yield client
+    finally:
+        await client.close()
+
+# Annotated type hint for dependency injection
+HttpClient = Annotated[AsyncHTTPClient, Depends(get_http_client)]
--- routers.py
+++ routers.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from api.controllers import Controller
 from api.controllers import AuthController
 from api.controllers import UserController
+from api.controllers import Ceisa40Controller
 from api.schemas import UserResponse
 from api.middleware import AuthBearer
 
@@ -9,6 +10,7 @@ router = APIRouter()
 controller = Controller()
 auth_controller = AuthController()
 user_controller = UserController()
+ceisa40_controller = Ceisa40Controller()
 
 router.get("/favicon.ico", include_in_schema=False)(controller.get_favicon)
 router.get("/")(controller.get_root)
@@ -21,3 +23,4 @@ router.post("/login")(auth_controller.login)
 # User
 router.post("/users")(user_controller.create_user)
 router.get("/users/{user_id}", response_model=UserResponse)(user_controller.get_user)
+router.get("/cek", dependencies=[Depends(AuthBearer())])(ceisa40_controller.get_data)
